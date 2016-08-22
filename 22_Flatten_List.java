3021c418fa00389fe9bc84d54c5a8379db16151e
Joshua Sun <joshuasun74@gmail.com>
2016-08-22 11:38:35 +0800

    Add #22 Flatten List

---
+++ 22_Flatten_List.java
@@ -0,0 +1,40 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer,
+ *     // rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds,
+ *     // if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds,
+ *     // if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+
+    // @param nestedList a list of NestedInteger
+    // @return a list of integer
+    public List<Integer> flatten(List<NestedInteger> nestedList) {
+        List<Integer> answer = new ArrayList<>();
+        if (nestedList == null) {
+            return answer;
+        }
+
+        for (NestedInteger nested : nestedList) {
+            if (nested.isInteger()) {
+                answer.add(nested.getInteger());
+            } else {
+                answer.addAll(flatten(nested.getList()));
+            }
+        }
+        return answer;
+    }
+}
